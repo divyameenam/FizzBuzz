4ed392557c46d082be36ffbc5a3a7c8e859cf572
Divya Meena M <mdmeena93@gmail.com>
2024-02-02 16:33:16 +0530

    Add project files.

---
+++ FizzBuzzAPI.Test/Services/FizzBuzzServiceTest.cs
@@ -0,0 +1,88 @@
+﻿using FizzBuzzAPI.Controllers;
+using FizzBuzzAPI.Model;
+using FizzBuzzAPI.Services;
+using FizzBuzzAPI.Utility;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzAPI.Test.Services
+{
+    [TestFixture]
+    public class FizzBuzzServiceTest
+    {
+        private readonly IFizzBuzzService fizzBuzzService;
+        private readonly Mock<IMathUtil> mockMathUtil;
+
+        public FizzBuzzServiceTest()
+        {
+            mockMathUtil = new Mock<IMathUtil>();
+            fizzBuzzService = new FizzBuzzService(mockMathUtil.Object);
+        }
+
+        [Test]
+        public void GetFizzBuzzResult_ReturnsFizz()
+        {
+            string[] input = new string[] { "3", "9", "33" };
+            mockMathUtil.Setup(m => m.Division(It.IsAny<int>(), It.IsAny<int>())).Returns<int,int>((a,b)=> a % b);
+
+            var result = fizzBuzzService.GetFizzBuzzResult(input);
+
+            Assert.IsNotNull(result);
+            Assert.That(result[0], Is.EqualTo("3 - " + Constants.strFizz));
+            Assert.That(result[1], Is.EqualTo("9 - " + Constants.strFizz));
+            Assert.That(result[2], Is.EqualTo("33 - " + Constants.strFizz));
+        }
+
+        [Test]
+        public void GetFizzBuzzResult_ReturnsBuzz()
+        {
+            string[] input = new string[] { "5", "25" };
+            mockMathUtil.Setup(m => m.Division(It.IsAny<int>(), It.IsAny<int>())).Returns<int, int>((a, b) => a % b);
+            var result = fizzBuzzService.GetFizzBuzzResult(input);
+
+            Assert.IsNotNull(result);
+            Assert.That(result[0], Is.EqualTo("5 - " + Constants.strBuzz));
+            Assert.That(result[1], Is.EqualTo("25 - " + Constants.strBuzz));
+        }
+
+        [Test]
+        public void GetFizzBuzzResult_ReturnsFizzBuzz()
+        {
+            string[] input = new string[] { "15", "75" };
+            mockMathUtil.Setup(m => m.Division(It.IsAny<int>(), It.IsAny<int>())).Returns<int, int>((a, b) => a % b);
+            var result = fizzBuzzService.GetFizzBuzzResult(input);
+
+            Assert.IsNotNull(result);
+            Assert.That(result[0], Is.EqualTo("15 - " + Constants.strFizzBuzz));
+            Assert.That(result[1], Is.EqualTo("75 - " + Constants.strFizzBuzz));
+        }
+
+        [Test]
+        public void GetFizzBuzzResult_ReturnsInvalidItem()
+        {
+            string[] input = new string[] { " ", "abc" };
+            mockMathUtil.Setup(m => m.Division(It.IsAny<int>(), It.IsAny<int>())).Returns<int, int>((a, b) => a % b);
+            var result = fizzBuzzService.GetFizzBuzzResult(input);
+
+            Assert.IsNotNull(result);
+            Assert.That(result[0], Is.EqualTo("  - " + Constants.strInvalidItem));
+            Assert.That(result[1], Is.EqualTo("abc - " + Constants.strInvalidItem));
+        }
+
+        [Test]
+        public void GetFizzBuzzResult_NotDivisibleBy3And5()
+        {
+            string[] input = new string[] { "1", "2" };
+            mockMathUtil.Setup(m => m.Division(It.IsAny<int>(), It.IsAny<int>())).Returns<int, int>((a, b) => a % b);
+            var result = fizzBuzzService.GetFizzBuzzResult(input);
+
+            Assert.IsNotNull(result);
+            Assert.That(result[0], Is.EqualTo("Divided 1 by 3 & Divided 1 by 5"));
+            Assert.That(result[1], Is.EqualTo("Divided 2 by 3 & Divided 2 by 5"));
+        }
+    }
+}
---
+++ FizzBuzzAPI/Services/FizzBuzzService.cs
@@ -0,0 +1,46 @@
+﻿using FizzBuzzAPI.Model;
+using FizzBuzzAPI.Utility;
+
+namespace FizzBuzzAPI.Services
+{
+    public class FizzBuzzService : IFizzBuzzService
+    {
+        private readonly IMathUtil _math;
+
+        public FizzBuzzService(IMathUtil math)
+        {
+            this._math = math;
+        }
+        public List<string> GetFizzBuzzResult(string[] input)
+        {
+            List<string> result = new List<string>();
+            int value;
+            foreach(var item in input)
+            {
+                if (int.TryParse(item, out value))
+                {
+                    if (_math.Division(value,3) == 0 && _math.Division(value, 5) == 0)
+                    {
+                        result.Add(item + " - " + Constants.strFizzBuzz);
+                    }
+                    else
+                    if (_math.Division(value, 3) == 0)
+                    {
+                        result.Add(item + " - " + Constants.strFizz);
+                    }
+                    else
+                    if (_math.Division(value, 5) == 0)
+                    {
+                        result.Add(item + " - " + Constants.strBuzz);
+                    }
+                    else
+                        result.Add("Divided " + item + " by 3 & Divided " + item + " by 5");
+                }
+                else
+                    result.Add(item + " - " + Constants.strInvalidItem);
+            }
+            
+            return result;
+        }
+    }
+}
